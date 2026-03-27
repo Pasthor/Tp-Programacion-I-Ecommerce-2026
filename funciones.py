5d62b70faaa9254ccc3ff83b2b564d09388a0d16
Pasthor <79120297+Pasthor@users.noreply.github.com>
2026-03-27 02:21:48 -0300

    Funcion comprar con efectivo 100% funcional.  Bucle while run

--- funciones.py
+++ funciones.py
@@ -1,9 +1,49 @@
-def mostrarLogo():
-    print("  ______  _____ ____  __  __ __  __ ______ _____   _____ ______ ")
-    print(" |  ____|/ ____/ __ \\|  \\/  |  \\/  |  ____|  __ \\ / ____|  ____|")
-    print(" | |__  | |   | |  | | \\  / | \\  / | |__  | |__) | |    | |__   ")
-    print(" |  __| | |   | |  | | |\\/| | |\\/| |  __| |  _  /| |    |  __|  ")
-    print(" | |____| |___| |__| | |  | | |  | | |____| | \\ \\| |____| |____ ")
-    print(" |______|\\_____\\____/|_|  |_|_|  |_|______|_|  \\_\\\\_____|______|")
+##UNIDADES A COMPRAR NO TIENE CONTROL DE ENTRADA1
 
-mostrarLogo()
+Run=True
+
+
+import funciones
+
+#Variables útiles
+relleno = "|-|-|-|-|-|-|"
+
+lineaPunt = "- - - - - - - - - - - - - - - - - - - - -"
+regre="Regresando..."
+confirmando= False
+comprando = False
+#Ecommerce
+##Listas y Listas Paralelas
+productos=      ["TV Samsung Led", "Ninja Blender","Helader con Freezer Philips", "Microondas Electrolux"]
+productosPrecio=[300,                     400,              350,                         120]
+productosStock= [40,                      32,               23,                          160]
+productosId=    [1,                        2,                3,                          4]
+opcionesMenu=["Comprar", "Ver productos", "Salir"]
+carrito=[]
+carritoTotal=0
+
+
+
+
+while Run==True:
+    opcion= funciones.MostrarMenu(opcionesMenu)
+    if opcion == 1: # COMPRAR
+        confirmando=False
+        comprando=True
+        while confirmando==False:
+            resultado=funciones.Comprar(carritoTotal, carrito, productos, productosPrecio, productosStock, comprando)
+            if resultado == "P":
+                confirmando=True
+            elif resultado == "SALIR":
+                break
+            elif resultado is not None:
+                carritoTotal = carritoTotal+resultado
+            else:
+                print("Caracter invalido")
+        if confirmando==True:
+            CompraRealizada=funciones.ConfirmarCompra(carrito, carritoTotal)
+            comprando=False
+            if CompraRealizada=="COMPRA NUEVA":
+                continue
+
+    
